71be1d3fc9f041c772b153c1aeef5c8e69436b6b
philiptang42 <philiptang42@gmail.com>
2015-12-26 23:21:56 -0500

    optional part 3 progression

--- person_constructor.rb
+++ person_constructor.rb
@@ -1,16 +1,22 @@
 require 'pry'
 
 class Person
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :middle_name
 
-  def initialize(first_name, last_name = nil)
-    if last_name.nil?
+  def initialize(first_name, last_name = nil, middle_name = nil)
+    if middle_name.nil?
       name_array = first_name.split(' ')
       @first_name = name_array[0]
       @last_name = name_array[1]
+    elsif last_name.nil?
+      name_array = first_name.split(' ')
+      @first_name = name_array[0]
+      @last_name = name_array[1]
+      @middle_name = name_array[2]
     else
       @first_name = first_name
       @last_name = last_name
+      @middle_name = middle_name
     end
   end
 
