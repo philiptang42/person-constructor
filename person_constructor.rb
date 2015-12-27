832d60eae2cb6134ab975c81dbd6b83faff4e2a8
philiptang42 <philiptang42@gmail.com>
2015-12-26 21:15:11 -0500

    part 2 solution

--- person_constructor.rb
+++ person_constructor.rb
@@ -3,9 +3,15 @@ require 'pry'
 class Person
   attr_reader :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(first_name, last_name = nil)
+    if last_name.nil?
+      name_array = first_name.split(' ')
+      @first_name = name_array[0]
+      @last_name = name_array[1]
+    else
+      @first_name = first_name
+      @last_name = last_name
+    end
   end
 
 end
